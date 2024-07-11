1a312dfb8951d296b41471b80490b9dfdaf2fd90
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2024-07-11 14:45:12 +0900

    03_01

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -9,11 +9,35 @@ public class Enemy : MonoBehaviour
     {
         Destroy(gameObject, 5);
         transform.rotation = Quaternion.Euler(0, 180, 0);
+
+        //—”‚Å¶‰E‚Ö
+        int r = Random.Range(0, 2);
+        if(r == 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180 - 30, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180 + 30, 0);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float movespeed = 2.0f;
+        Vector3 velocity = new Vector3(0, 0, movespeed * Time.deltaTime);
+        transform.position += transform.rotation * velocity;
+
+        //¶‰E‚Å”½“]
+        if(transform.position.x >4)
+        {
+            transform.rotation = Quaternion.Euler(0, 180 + 30, 0);
+        }
+        if(transform.position.x < -4)
+        {
+            transform.rotation = Quaternion.Euler(0, 180 - 30, 0);
+        }
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,12 +8,23 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemy, new Vector3(2, 2, 3), Quaternion.identity);
+        float x = Random.Range(-3.0f, 3.0f);
+        Instantiate(enemy, new Vector3(x, 2, 3), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        int r = Random.Range(0, 50);
+        if(r == 0)
+        {
+            float x = Random.Range(-3.0f, 3.0f);
+            Instantiate(enemy, new Vector3(x, 2, 4), Quaternion.identity);
+        }
     }
 }
