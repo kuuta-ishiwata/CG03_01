0c2e993f0542135c4bd3de5dd261d742f3b1c5f8
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2024-07-11 13:08:21 +0900

    enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemy, new Vector3(2, 2, 3), Quaternion.identity);
+        Destroy(gameObject, 5);
+        transform.rotation = Quaternion.Euler(0, 180, 0);
     }
 
     // Update is called once per frame
