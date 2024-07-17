f1109bda7501b9ac07263e079ead97d51e1006cd
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2024-07-17 15:26:36 +0900

    CG

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -8,6 +8,7 @@ public class GameManagerScript : MonoBehaviour
 {
     public GameObject enemy;
     public GameObject gameOverText;
+    public AudioSource hitAudioSource;
     private bool gameOverFlag = false;
     // Start is called before the first frame update
     void Start()
